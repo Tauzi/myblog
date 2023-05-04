5de2e795215a01aa7874a3737a485af8578e9524
TauZi <1223156656@qq.com>
2023-05-04 20:56:18 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 hugo #构造你的blog
 
+cd public
+
 git init #初始化git
 
 git add -A
 
-git commit -m 'deploy'
+git commit -m 'deploy123'
 
-git push -f git@github.com:Tauzi/myblog.git main  #向存储库推送
+git push -f git@github.com:Tauzi/myblog.git master:gh-pages    #向存储库推送
